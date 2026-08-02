67113cfa2378ca949f0bb53f37d811e4403e2cfc
Rahma Ahmed <rahmaaahmedd10@gmail.com>
2026-08-02 16:03:54 +0300

    Q2: solve Question 2

--- G-ASP-NET-83-C#Basics03/Program.cs
+++ G-ASP-NET-83-C#Basics03/Program.cs
@@ -9,6 +9,15 @@
             double[] prices = { 25.5, 40.0, 33.75 };
             Console.WriteLine($"Second price: {prices[1]}");
             #endregion
+            #region Question 02
+            // Create a 2x2 multidimensional array int[,] shelfCopies. Print shelf 1, slot 0.
+            int[,] shelfCopies = {
+                     { 3, 5 },
+                     { 1, 4 }
+                         };
+            Console.WriteLine($"Shelf 1, Slot 0 copies: {shelfCopies[1, 0]}");
+            #endregion
+
         }
     }
 }
