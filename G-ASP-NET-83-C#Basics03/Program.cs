746885a25eb4a6f4749532252c11e3ba2362f555
Rahma Ahmed <rahmaaahmedd10@gmail.com>
2026-08-02 15:44:06 +0300

    Add project files.

---
+++ G-ASP-NET-83-C#Basics03/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace G_ASP_NET_83_C_Basics03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
