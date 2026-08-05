3e9f0f474ffe17c72e12b7ba6b4a09dd826c4368
Rahma Ahmed <rahmaaahmedd10@gmail.com>
2026-08-05 17:21:12 +0300

    Q4: solve Question 4

--- G-ASP-NET-83-C#Basics03/Program.cs
+++ G-ASP-NET-83-C#Basics03/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
 
 namespace G_ASP_NET_83_C_Basics03
 {
@@ -11,12 +11,13 @@ namespace G_ASP_NET_83_C_Basics03
             double[] prices = { 25.5, 40.0, 33.75 };
             Console.WriteLine($"Second price: {prices[1]}");
             #endregion
+
             #region Question 02
             // Create a 2x2 multidimensional array int[,] shelfCopies. Print shelf 1, slot 0.
             int[,] shelfCopies = {
-                     { 3, 5 },
-                     { 1, 4 }
-                         };
+                { 3, 5 },
+                { 1, 4 }
+            };
             Console.WriteLine($"Shelf 1, Slot 0 copies: {shelfCopies[1, 0]}");
             #endregion
 
@@ -24,20 +25,28 @@ namespace G_ASP_NET_83_C_Basics03
             // Write a method PrintWelcomeMessage that prints "Welcome to the Library!". Call it from Main.
             PrintWelcomeMessage();
             #endregion
-        }
+
+            #region Question 04
+            // Write a method PrintBookTitle(string title) that prints "Book title: " + title. Call it with "Clean Code".
+            PrintBookTitle("Clean Code");
+            #endregion
+
+        } 
 
         #region Methods Definitions
 
-
+        
         public static void PrintWelcomeMessage()
         {
             Console.WriteLine("Welcome to the Library!");
         }
 
+        
+        public static void PrintBookTitle(string title)
+        {
+            Console.WriteLine("Book title: " + title);
+        }
+
         #endregion
-      
-
-
     }
 }
-
